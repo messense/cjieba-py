0c7a2023e199a7125e86057ebedf691429b2eb6a
messense <messense@icloud.com>
2018-01-14 18:23:47 +0800

    Bump version: 0.3.0 → 0.3.1

--- cjieba/__init__.py
+++ cjieba/__init__.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 
 from ._native import ffi, lib
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 CURR_PATH = path.abspath(path.dirname(__file__))
 PY2 = sys.version_info[0] == 2
