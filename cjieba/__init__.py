4a2940c3a35d4f0be6fb30f4a3ce2c55bb7121c5
messense <messense@icloud.com>
2020-09-15 16:59:34 +0800

    Bump version: 0.4.3 → 0.4.4

--- cjieba/__init__.py
+++ cjieba/__init__.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 
 from ._native import ffi, lib
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 CURR_PATH = path.abspath(path.dirname(__file__))
 PY2 = sys.version_info[0] == 2
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def build_native(spec):
 
 setup(
     name='cjieba',
-    version='0.4.3',
+    version='0.4.4',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/messense/cjieba-py',
